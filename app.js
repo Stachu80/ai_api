c0d805f96f06a0356594d04aba8e61b68e889f28
mstasinski <mstasinski@clickmeeting.com>
2024-04-12 23:59:22 +0200

    port

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 app.listen(port, () => {
     console.log(`API Mock running at http://localhost:${port}`);
